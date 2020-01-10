391e1244f744e4b2ec03d80f2d28d1361388594c
Marisha Gnanaseelan <magn@nmbu.no>
2020-01-10 17:07:46 +0100

    Corrected rossumoya.py

--- BioSim_G21_Julie_Marisha/src/biosim/rossumoya.py
+++ BioSim_G21_Julie_Marisha/src/biosim/rossumoya.py
@@ -4,7 +4,6 @@
 """
 from BioSim_G21_Julie_Marisha.src.biosim.animal import Herbivore, Carnivore
 from BioSim_G21_Julie_Marisha.src.biosim.cell import Savannah, Jungle, Desert, MountainAndOcean
-from BioSim_G21_Julie_Marisha.src.biosim.simulation import BioSim
 
 __author__ = 'Julie Forrisdal', 'Marisha Gnanaseelan'
 __email__ = 'juforris@nmbu.no', 'magn@nmbu.no'
@@ -19,10 +18,8 @@ class Rossumoya:
     default_ini_herbs = [
         {
             "loc": (10, 10),
-            "pop": [
-                {"species": "Herbivore", "age": 5, "weight": 20}
-                for _ in range(150)
-            ],
+            "pop": [{"species": "Herbivore", "age": 5, "weight": 20}
+                    for _ in range(150)],
         }
     ]
     default_ini_carns = [
@@ -53,12 +50,15 @@ class Rossumoya:
 
     cell_code = {"S": Savannah, "J": Jungle, "D": Desert, "O": MountainAndOcean, "M": MountainAndOcean}
 
-    def __init__(self, island_map, ini_pop):
+    def __init__(self, island_map=None, ini_pop=None):
         if island_map is None:
             self.island_map = Rossumoya.default_map
-        if self.check_map_input(island_map):
-            self.island_map = island_map
             self.island_map = self.make_geography_coordinates(self.island_map)
+
+        else:
+            if self.check_map_input(island_map):
+                self.island_map = island_map
+                self.island_map = self.make_geography_coordinates(self.island_map)
 
         if ini_pop is None:
             self.add_population(Rossumoya.default_ini_herbs)
@@ -102,12 +102,11 @@ class Rossumoya:
 
         :return: dict
         """
-        list_island_map = self.default_map.split('\n')
+        list_island_map = input_map.split('\n')
         geography_map = {}
         for i_index, line in enumerate(list_island_map):
             for j_index, cell in enumerate(line):
-                geography_map[(i_index + 1, j_index + 1)] = Rossumoya.cell_code[cell](),
-
+                geography_map[(i_index + 1, j_index + 1)] = Rossumoya.cell_code[cell]()
         return geography_map
 
     def add_population(self, population):
