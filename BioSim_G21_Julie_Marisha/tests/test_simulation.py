c42aa71654faaabad9a36e4722d61811f265b195
Marisha Gnanaseelan <magn@nmbu.no>
2020-01-11 15:20:28 +0100

    Test for default animal distribution

--- BioSim_G21_Julie_Marisha/tests/test_simulation.py
+++ BioSim_G21_Julie_Marisha/tests/test_simulation.py
@@ -9,9 +9,18 @@ __email__ = 'juforris@nmbu.no', 'magn@nmbu.no'
 
 
 from BioSim_G21_Julie_Marisha.src.biosim.simulation import BioSim
+import pandas as pd
+
 
 class TestBiosim:
     def test_default_animal_distribution(self):
+        """Default animal distribution in cells are created correctly"""
         sim1 = BioSim()
         data_frame = sim1.animal_distribution
-        print(data_frame['total_herbivores'][[(10, 10)]])
+        num_herbivore_list = data_frame['total_herbivores'][[(10, 10)]]
+        assert num_herbivore_list.to_list() == [150]
+
+        num_carnivore_list = data_frame['total_carnivores'][[(10, 10)]]
+        assert num_carnivore_list.to_list() == [40]
+
+
