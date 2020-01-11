8b6b47b5b47fde51f0ea4a0c2391f24dae01ebc6
Marisha Gnanaseelan <magn@nmbu.no>
2020-01-11 15:46:49 +0100

    Added tests for class BioSim

--- BioSim_G21_Julie_Marisha/tests/test_simulation.py
+++ BioSim_G21_Julie_Marisha/tests/test_simulation.py
@@ -9,12 +9,28 @@ __email__ = 'juforris@nmbu.no', 'magn@nmbu.no'
 
 
 from BioSim_G21_Julie_Marisha.src.biosim.simulation import BioSim
-import pandas as pd
 
 
 class TestBiosim:
+    """Tests for class BioSim."""
+    def test_year_before_first_simulation(self):
+        """Year function return 0 if there has been no simulations. """
+        d = BioSim()
+        assert d.year == 0
+
+    def test_default_num_animals(self):
+        """Test that default number of animals before the first simulation
+        is created correctly."""
+        d = BioSim()
+        assert d.num_animals == 190
+
+    def test_num_animals_per_species(self):
+        """Test that num_animals_per_species returns a dictionary."""
+        d = BioSim()
+        assert isinstance(d.num_animals_per_species, dict)
+
     def test_default_animal_distribution(self):
-        """Default animal distribution in cells are created correctly"""
+        """Default animal distribution in cells are created correctly."""
         sim1 = BioSim()
         data_frame = sim1.animal_distribution
         num_herbivore_list = data_frame['total_herbivores'][[(10, 10)]]
@@ -22,5 +38,3 @@ class TestBiosim:
 
         num_carnivore_list = data_frame['total_carnivores'][[(10, 10)]]
         assert num_carnivore_list.to_list() == [40]
-
-
