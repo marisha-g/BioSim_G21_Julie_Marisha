c7de50959996d2cbc33805a871bf47f0223a81d8
Julie Wollebæk Førrisdal <juforris@nmbu.no>
2020-01-13 16:10:43 +0100

    propensity calculator, more dockstrings and cleanup

--- BioSim_G21_Julie_Marisha/src/biosim/rossumoya.py
+++ BioSim_G21_Julie_Marisha/src/biosim/rossumoya.py
@@ -5,12 +5,40 @@
 import numpy as np
 
 from BioSim_G21_Julie_Marisha.src.biosim.animal import Herbivore, Carnivore
-from BioSim_G21_Julie_Marisha.src.biosim.cell import Savannah, Jungle, Desert, MountainAndOcean
+from BioSim_G21_Julie_Marisha.src.biosim.cell import Savannah, \
+    Jungle, Desert, MountainAndOcean
 
 __author__ = 'Julie Forrisdal', 'Marisha Gnanaseelan'
 __email__ = 'juforris@nmbu.no', 'magn@nmbu.no'
 
 import textwrap
+
+
+class PropensityCalculator:
+    def __init__(self, locs, island_map):
+        """
+        Constructor initiate PropensityCalculator
+        :param locs:
+        :type: list
+        :param island_map:
+        :type: dict
+        """
+        self.locations = locs
+        self.island_map = island_map
+
+    def herbs(self):
+        propensities = []
+        for cell in self.locations:
+            cell = self.island_map[cell]
+            propensities.append(cell.propensity_migration_herb)
+        return propensities
+
+    def carns(self):
+        propensities = []
+        for cell in self.locations:
+            cell = self.island_map[cell]
+            propensities.append(cell.propensity_migration_carn)
+        return propensities
 
 
 class Rossumoya:
@@ -140,9 +168,13 @@ class Rossumoya:
                 age = pop_dict['age']
                 weight = pop_dict['weight']
                 if species == 'Herbivore':
-                    self.island_map[location].animals.append(Herbivore(age, weight))
+                    self.island_map[location].animals.append(
+                        Herbivore(age, weight)
+                    )
                 if species == 'Carnivore':
-                    self.island_map[location].animals.append(Carnivore(age, weight))
+                    self.island_map[location].animals.append(
+                        Carnivore(age, weight)
+                    )
 
     def procreation(self):
         """
@@ -154,18 +186,27 @@ class Rossumoya:
             if cell.animal_can_enter:
                 for animal in cell.animals:
                     species = type(animal).__name__
+
                     if species == 'Herbivore':
-                        animal_gives_birth = animal.prob_procreation(cell.total_herbivores)
+                        animal_gives_birth = animal.prob_procreation(
+                            cell.total_herbivores
+                        )
                     if species == 'Carnivore':
-                        animal__gives_birth = animal.prob_procreation(cell.total_carnivores)
+                        animal__gives_birth = animal.prob_procreation(
+                            cell.total_carnivores
+                        )
 
                     if animal_gives_birth:
-                        weight = animal.draw_birth_weight()
-                        self.add_population([{'loc': loc,
-                                              'pop': {
-                                                     'species': species,
-                                                     'age': 0,
-                                                     'weight': weight}}])
+                        self.animal_give_birth(animal, loc)
+
+    def animal_give_birth(self, animal, loc):
+        weight = animal.draw_birth_weight()
+        baby = [{'loc': loc,
+                'pop': {
+                    'species': type(animal).__name__,
+                    'age': 0,
+                    'weight': weight}}]
+        self.add_population(baby)
 
     def migration(self):
         """
@@ -187,7 +228,7 @@ class Rossumoya:
         """
         Calculate propensities and returns
         coordinates of chosen cell to migrate to.
-        :param: loc: Location coordinates before migration
+        :param: loc: Location coordinates before migration.
         :type: tuple
         :param: species: Herbivore or Carnivore
         :type: str
@@ -202,12 +243,12 @@ class Rossumoya:
         locations = [cell_left, cell_right, cell_up, cell_down]
 
         if species == 'Herbivore':
-            calculate_propensities_herb = self.propensity_calculator('Herbivore')
-            propensities = calculate_propensities_herb(locations)
+            calculator = PropensityCalculator(locations, self.island_map)
+            propensities = calculator.herbs()
 
         if species == 'Carnivore':
-            calculate_propensities_carn = self.propensity_calculator('Carnivore')
-            propensities = calculate_propensities_carn(locations)
+            calculate_prop_carn = self.propensity_calculator('Carnivore')
+            propensities = calculate_prop_carn(locations)
 
         sum_propensities = sum(propensities)
         pl = propensities[0] / sum_propensities * propensities[0]
@@ -218,23 +259,10 @@ class Rossumoya:
         choice = np.random.choice(locations, p=[pl, pr, pu, pd])
         return choice
 
-    def propensity_calculator(self, species):
-        propensities = []
-        if species == 'Herbivore':
-            def calculator(locations):
-                for cell in locations:
-                    propensities.append(self.island_map[cell].propensity_migration_herb)
-                return propensities
-            return calculator
-
-        if species == 'Carnivore':
-            def calculator(locations):
-                for cell in locations:
-                    propensities.append(self.island_map[cell].propensity_migration_carn)
-                return propensities
-            return calculator
-
     def death(self):
+        """
+        Animals are removed if prob_death returns 1.
+        """
         for cell in self.island_map.values():
             dead_animals = []
             for animal in cell.animals:
@@ -246,8 +274,8 @@ class Rossumoya:
     @staticmethod
     def make_geography_coordinates(input_map):
         """
-        Makes a dictionary with coordinates as keys and Cell subclass instances
-        (Savannah, Jungle, Desert, MountainAndOcean) as values,
+        Makes a dictionary with coordinates as keys and Cell subclass
+        instances (Savannah, Jungle, Desert, MountainAndOcean) as values,
         initiated with default parameters.
         :return: geography_map
         :type: dict
@@ -256,5 +284,6 @@ class Rossumoya:
         geography_map = {}
         for i_index, line in enumerate(list_island_map):
             for j_index, cell in enumerate(line):
-                geography_map[(i_index, j_index)] = Rossumoya.cell_code[cell]()
+                cell_instance = Rossumoya.cell_code[cell]
+                geography_map[(i_index, j_index)] = cell_instance()
         return geography_map
