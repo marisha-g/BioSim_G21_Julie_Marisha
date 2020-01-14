11f5fce4e575faf651a395da6daaa907f36b788f
Marisha Gnanaseelan <magn@nmbu.no>
2020-01-14 12:07:42 +0100

    Added tests for class MigrationProbabilityCalculator()

--- BioSim_G21_Julie_Marisha/tests/test_rossumoya.py
+++ BioSim_G21_Julie_Marisha/tests/test_rossumoya.py
@@ -8,9 +8,40 @@ __author__ = 'Julie Forrisdal', 'Marisha Gnanaseelan'
 __email__ = 'juforris@nmbu.no', 'magn@nmbu.no'
 
 from src.biosim.rossumoya import Rossumoya
-import textwrap
+from BioSim_G21_Julie_Marisha.src.biosim.rossumoya import MigrationProbabilityCalculator
+from BioSim_G21_Julie_Marisha.src.biosim.animal import Herbivore, Carnivore
 import pytest
 
+
+class TestMigrationProbabilityCalculator:
+    """Tests for class MigrationProbabilityCalculator."""
+
+    @pytest.fixture(autouse=True)
+    def create_calculator(self):
+        self.loc = (2, 2)
+        self.map = Rossumoya.make_geography_coordinates(Rossumoya.default_map)
+        Herbivore.set_parameters()
+        Carnivore.set_parameters()
+        self.species = "Herbivore"
+        self.calculator = MigrationProbabilityCalculator(
+            self.loc, self.map, self.species
+        )
+
+    def test_constructor_callable(self):
+        """MigrationProbabilityCalculator is callable. """
+        assert isinstance(self.calculator, MigrationProbabilityCalculator)
+
+    def test_propensity_herb(self):
+        """propensity_herb() method returns a list."""
+        assert isinstance(self.calculator.propensity_herb(), list)
+
+    def test_propensity_herb(self):
+        """propensity_carns() method returns a list."""
+        assert isinstance(self.calculator.propensity_carns(), list)
+
+    def test_probability(self):
+        """probability() method returns a tuple and a list."""
+        assert isinstance(self.calculator.probability(), (tuple, list))
 
 
 class TestRossumoya:
@@ -35,3 +66,5 @@ class TestRossumoya:
         """add_population can be called."""
         c = Rossumoya()
         c.add_population(Rossumoya.default_ini_herbs)
+
+
