5ec50d68b7176edce4130b95d334b41ae6d92df6
Julie Wollebæk Førrisdal <juforris@nmbu.no>
2020-01-14 16:33:12 +0100

    Added pytest.fixture

--- BioSim_G21_Julie_Marisha/tests/test_simulation.py
+++ BioSim_G21_Julie_Marisha/tests/test_simulation.py
@@ -13,26 +13,26 @@ from biosim.simulation import BioSim
 
 class TestBiosim:
     """Tests for class BioSim."""
+    @pytest.fixture
+    def create_sim(self):
+        self.biosim = BioSim()
+
     def test_year_before_first_simulation(self):
-        """Year function return 0 if there has been no simulations. """
-        d = BioSim()
-        assert d.year == 0
+        """Year property return 0 if there has been no simulations. """
+        assert self.biosim.year == 0
 
     def test_default_num_animals(self):
         """Test that default number of animals before the first simulation
         is created correctly."""
-        d = BioSim()
-        assert d.num_animals == 190
+        assert self.biosim.num_animals == 190
 
     def test_num_animals_per_species(self):
         """Test that num_animals_per_species returns a dictionary."""
-        d = BioSim()
-        assert isinstance(d.num_animals_per_species, dict)
+        assert isinstance(self.biosim.num_animals_per_species, dict)
 
     def test_default_animal_distribution(self):
         """Default animal distribution in cells are created correctly."""
-        sim1 = BioSim()
-        data_frame = sim1.animal_distribution
+        data_frame = self.biosim.animal_distribution
         data_frame.set_index(["Row", "Col"], inplace=True)
         assert data_frame.loc[(10, 10)].Herbivore == 150
         assert data_frame.loc[(10, 10)].Carnivore == 40
