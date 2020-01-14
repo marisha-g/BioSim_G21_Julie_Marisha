2689175d7e497f8ce394271041d6023e05bcbab9
Julie Wollebæk Førrisdal <juforris@nmbu.no>
2020-01-14 17:10:50 +0100

    test for set_animal_parameters

--- BioSim_G21_Julie_Marisha/tests/test_simulation.py
+++ BioSim_G21_Julie_Marisha/tests/test_simulation.py
@@ -9,6 +9,7 @@ __email__ = 'juforris@nmbu.no', 'magn@nmbu.no'
 
 
 from biosim.simulation import BioSim
+from biosim.animal import Herbivore, Carnivore
 import pytest
 
 
@@ -17,6 +18,13 @@ class TestBiosim:
     @pytest.fixture(autouse=True)
     def create_sim(self):
         self.biosim = BioSim()
+        self.population = population = [
+        {
+            "loc": (10, 10),
+            "pop": [{"species": "Herbivore", "age": 5, "weight": 20}
+                    for _ in range(150)],
+        }
+    ]
 
     def test_year_before_first_simulation(self):
         """Year property returns 0 before simulation. """
@@ -44,3 +52,22 @@ class TestBiosim:
         data_frame.set_index(["Row", "Col"], inplace=True)
         assert data_frame.loc[(10, 10)].Herbivore == 150
         assert data_frame.loc[(10, 10)].Carnivore == 40
+
+    def test_set_animal_parameters_callabe(self):
+        """set_animal_parameters method can be called."""
+        params = {}
+        self.biosim.set_animal_parameters('Herbivore', params)
+
+    def test_set_animal_parameters(self):
+        """set_animal_parameters method changes parameters of animal class
+        correctly."""
+        params1 = {'gamma': 0.5}
+        params2 = {'xi': 0.4}
+        self.biosim.set_animal_parameters('Herbivore', params1)
+        self.biosim.set_animal_parameters('Carnivore', params2)
+        assert Herbivore.gamma == 0.5
+        assert Carnivore.xi == 0.4
+
+    def test_add_population(self):
+        """ add_population method can be called."""
+        self.biosim.add_population(self.population)
