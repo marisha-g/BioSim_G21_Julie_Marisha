2958b12038dfc6b0374384b46223f9f3390d55fb
Marisha Gnanaseelan <magn@nmbu.no>
2020-01-15 13:19:40 +0100

    Headings

---
+++ BioSim_G21_Julie_Marisha/src/biosim/visualization.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+__author__ = 'Julie Forrisdal', 'Marisha Gnanaseelan'
+__email__ = 'juforris@nmbu.no', 'magn@nmbu.no'
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+_DEFAULT_MOVIE_FORMAT = 'mp4'
+
+
+class BioVis:
+    """Provides user interface for simulation, including visualization."""
+    
---
+++ BioSim_G21_Julie_Marisha/tests/test_visualization.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tests for classes in visualization.py using pytest.
+"""
+
+__author__ = 'Julie Forrisdal', 'Marisha Gnanaseelan'
+__email__ = 'juforris@nmbu.no', 'magn@nmbu.no'
+
+import pytest
