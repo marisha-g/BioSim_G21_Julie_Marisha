9ac75ba2591b422795192d3cf7cc06c268b86fbf
Julie Wollebæk Førrisdal <juforris@nmbu.no>
2020-01-16 12:24:16 +0100

    biosim_movie.py

---
+++ BioSim_G21_Julie_Marisha/examples/biosim_movie.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+This is a small demo script running a ranvis simulation and generating a movie.
+"""
+
+__author__ = "Hans E Plesser / NMBU"
+
+from randvis.simulation import DVSim
+
+sim = DVSim((10, 15), 0.1, 12345, '../data')
+sim.simulate(100, 1, 5)
+sim.make_movie('mp4')
+sim.make_movie('gif')
