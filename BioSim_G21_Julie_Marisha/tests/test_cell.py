e78894bbf1454aa8ded227756fd2a0bf1938f48b
Marisha Gnanaseelan <magn@nmbu.no>
2020-01-16 15:26:29 +0100

    Added new tests for class instances in cell.py

--- BioSim_G21_Julie_Marisha/tests/test_cell.py
+++ BioSim_G21_Julie_Marisha/tests/test_cell.py
@@ -182,6 +182,7 @@ class TestSavannah:
     @pytest.fixture(autouse=True)
     def create_cell(self):
         self.s = Savannah()
+        Savannah.set_parameters()
 
     def test_constructor(self):
         """Default constructor is callable. """
@@ -199,6 +200,12 @@ class TestSavannah:
             Savannah.set_parameters(f_max=-100)
         with pytest.raises(ValueError):
             Savannah.set_parameters(alpha=-0.3)
+
+    def test_regrow_fodder_savannah(self):
+        """Formula for regrow fodder in Savannah is correct."""
+        self.s.fodder_in_cell = 20
+        self.s.regrow_fodder()
+        assert self.s.fodder_in_cell == 104
 
 
 class TestJungle:
@@ -261,6 +268,13 @@ class TestMountain:
         assert self.m.f_max == 0
         assert self.m.animal_can_enter is False
 
+    def test_propensity_migration_herb(self):
+        """ """
+        assert self.m.propensity_migration_herb == 0
+
+    def test_propensity_migration_carn(self):
+        """ """
+        assert self.m.propensity_migration_carn == 0
 
 class TestOcean:
     """ Tests for Ocean class."""
@@ -277,3 +291,11 @@ class TestOcean:
         assert self.o.fodder_in_cell == 0
         assert self.o.f_max == 0
         assert self.o.animal_can_enter is False
+
+    def test_propensity_migration_herb(self):
+        """ """
+        assert self.o.propensity_migration_herb == 0
+
+    def test_propensity_migration_carn(self):
+        """ """
+        assert self.o.propensity_migration_carn == 0
