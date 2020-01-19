de200a51c65d2f880d139b3f595f3c102cf54104
Julie Wollebæk Førrisdal <juforris@nmbu.no>
2020-01-19 23:13:14 +0100

    small corrections

--- BioSim_G21_Julie_Marisha/tests/test_simulation.py
+++ BioSim_G21_Julie_Marisha/tests/test_simulation.py
@@ -8,12 +8,10 @@ __author__ = 'Julie Forrisdal', 'Marisha Gnanaseelan'
 __email__ = 'juforris@nmbu.no', 'magn@nmbu.no'
 
 import os
-import textwrap
-
 import pytest
-import matplotlib.pyplot as plt
+from biosim.rossumoya import MigrationProbabilityCalculator
 
-from biosim.simulation import BioSim, MigrationProbabilityCalculator
+from biosim.simulation import BioSim
 from biosim.animal import Herbivore, Carnivore
 from biosim.cell import Savannah, Jungle
 
@@ -57,8 +55,8 @@ class TestBiosim:
         """Default animal distribution in cells are created correctly."""
         data_frame = self.biosim.animal_distribution
         data_frame.set_index(["Row", "Col"], inplace=True)
-        assert data_frame.loc[(10, 10)].Herbivore == 150
-        assert data_frame.loc[(10, 10)].Carnivore == 40
+        assert data_frame.loc[(10, 13)].Herbivore == 150
+        assert data_frame.loc[(10, 13)].Carnivore == 40
 
     def test_set_animal_parameters_callable(self):
         """set_animal_parameters method can be called."""
@@ -88,23 +86,6 @@ class TestBiosim:
     def test_add_population_callable(self):
         """ add_population method can be called."""
         self.biosim.add_population(self.population)
-
-    def test_migration(self):
-        """Test that animals migrate away from current cell when probability
-        to migrate is set to 1."""
-        island_map = """OOOOO\nOOJOO\nOJSJO\nOOJOO\nOOOOO"""
-        ini_pop = [{
-            "loc": (2, 2),
-            "pop": [{"species": "Herbivore", "age": 5, "weight": 20}
-                    for _ in range(150)],
-        }]
-        sim1 = BioSim(island_map=island_map, ini_pop=ini_pop)
-        MigrationProbabilityCalculator.probabilities = [0.5, 0.5, 0, 0]
-        Herbivore.prob_migration = 1
-        Carnivore.prob_migration = 1
-        sim1.simulate(num_years=1, vis_years=1)
-        cell = sim1.rossumoya.island_map[(2, 2)]
-        assert cell.total_population == 0
 
     def test_simulate_callable(self):
         """ simulate method can be called."""
