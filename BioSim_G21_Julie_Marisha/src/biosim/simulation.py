c02ba1c7afcc404f1f2dd9ac70e666324091153d
Julie Wollebæk Førrisdal <juforris@nmbu.no>
2020-01-21 10:59:07 +0100

    fixed main block

--- BioSim_G21_Julie_Marisha/src/biosim/simulation.py
+++ BioSim_G21_Julie_Marisha/src/biosim/simulation.py
@@ -519,17 +519,9 @@ class BioSim:
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     sim1 = BioSim(img_base=DEFAULT_IMAGE_BASE, ymax_animals=500)
 
-    sim1.simulate(num_years=5, vis_years=1, img_years=1)
+    sim1.simulate(num_years=50, vis_years=1, img_years=1)
     sim1.make_movie()
 
-=======
-    sim1 = BioSim()
-    sim1.simulate(num_years=100, vis_years=2, img_years=5)
-    sim1 = BioSim(img_base=DEFAULT_IMAGE_BASE)
-    sim1.simulate(num_years=2, vis_years=2, img_years=5)
-    sim1.make_movie()
->>>>>>> master
-    plt.show()
+
